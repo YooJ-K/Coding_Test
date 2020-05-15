00c84085c161c7991d290ab52c74525be8e43467
YooJ-K <kimyujin173@gmail.com>
2020-05-15 15:37:24 +0900

    version 1.1

--- 2748.cpp
+++ 2748.cpp
@@ -7,10 +7,11 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 0; i <= n; i++) {
-		if (i == 0) arr[i] = 0;
-		else if (i == 1) arr[i] = 1;
-		else arr[i] = arr[i - 1] + arr[i - 2];
+	arr[0] = 0;
+	arr[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
 	}
 
 	cout << arr[n] << endl;
