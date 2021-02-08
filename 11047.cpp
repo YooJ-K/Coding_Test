309f39ddf1048bcc8de1c77dae1c2656a7e8aa4c
YooJ-K <kimyujin173@gmail.com>
2021-02-09 04:59:05 +0900

    백준 11047번 문제

--- 11047.cpp
+++ 11047.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main() {
-	int n, k, count = 0, *a;
+	int n, k, count = 0, * a;
 
 	cin >> n >> k;
+
+	// 저장 공간을 할당해주는 이유? 
+	// 오름 차순으로 입력이 들어오기 때문이다.
 	a = new int[n];
 
-	for (int i = n-1; i >= 0; i--)
+
+	for (int i = n - 1; i >= 0; i--)
 	{
 		cin >> a[i];
 	}
