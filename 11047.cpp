9ac86a07e63e170753c5c4f0fd589297df6558b1
YooJ-K <chris3209@naver.com>
2021-02-09 04:54:54 +0900

    백준 11047번 문제

---
+++ 11047.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k, count = 0, *a;
+
+	cin >> n >> k;
+	a = new int[n];
+
+	for (int i = n-1; i >= 0; i--)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (k == k % a[i]) continue;
+		count += (k / a[i]);
+		k %= a[i];
+	}
+
+	cout << count << endl;
+}
